1d9d44776baed83a5a47184b4d367811fd17ef00
Jacques <jacques@trixta.com>
2021-03-18 10:04:20 +0200

    fix: include missing exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+export * from './actions/index';
 export * as actions from './actions/index';
+export * from './constants';
 export * as constants from './constants';
+export * from './middlewares';
 export * as middlewares from './middlewares';
+export * from './reducers';
 export * as reducers from './reducers';
+export * from './selectors';
 export * as selectors from './selectors';
-export { formatSocketDomain } from './utils';
+export * from './utils/index';
 export * as utils from './utils/index';
-
 

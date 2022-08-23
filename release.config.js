9c1ef633e45244d84a66612a03d6068d5af6ae0a
Jacques Nel <jacqueswho@gmail.com>
2022-08-23 11:41:07 +0200

    ci: workflows added

---
+++ release.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { revert: true, release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { scope: 'no-release', release: false },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ['@semantic-release/npm', { npmPublish: true }],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md', 'badges/**/*'],
+        message: 'chore(release): @trixtateam/phoenix-to-redux@v${nextRelease.version}',
+      },
+    ],
+  ],
+};

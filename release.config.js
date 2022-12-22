83eb0d7b63f215dd6bade6e80539ada7be82813d
Jacques Nel <jacqueswho@gmail.com>
2022-12-22 08:53:01 +0200

    ci: update to automatically publish library

--- release.config.js
+++ release.config.js
@@ -29,7 +29,7 @@ module.exports = {
     ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ['@semantic-release/npm', { npmPublish: true }],
+    ['@semantic-release/npm', { npmPublish: false }],
     [
       '@semantic-release/git',
       {
